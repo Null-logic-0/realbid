f9089873e0e54409ed1322efe08c0ec5648165a8
Luka Tchelidze <157907877+Null-logic-0@users.noreply.github.com>
2025-10-19 16:36:24 +0400

    Update production config and remove credentials file
    Deleted the encrypted production credentials file and updated production.rb to use Rails.application.credentials.secret_key_base. Also disabled Rubocop inspection in the IDE profile.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,45 +1,45 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  # Reloading
-  config.enable_reloading = false
-  config.eager_load = true
+	# Reloading
+	config.enable_reloading = false
+	config.eager_load = true
 
-  # Error reports
-  config.consider_all_requests_local = false
+	# Error reports
+	config.consider_all_requests_local = false
 
-  # Secret key
-  config.secret_key_base = Rails.application.credentials.dig(:production, :secret_key_base)
+	# Secret key
+	config.secret_key_base = Rails.application.credentials.secret_key_base
 
-  # Caching
-  config.action_controller.perform_caching = true
-  config.cache_store = :memory_store # simple default
+	# Caching
+	config.action_controller.perform_caching = true
+	config.cache_store = :memory_store # simple default
 
-  # Static files
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
-  config.public_file_server.headers = { "Cache-Control" => "public, max-age=#{1.year.to_i}" }
+	# Static files
+	config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+	config.public_file_server.headers = { "Cache-Control" => "public, max-age=#{1.year.to_i}" }
 
-  # SSL
-  config.force_ssl = true
-  config.assume_ssl = true
+	# SSL
+	config.force_ssl = true
+	config.assume_ssl = true
 
-  # Logging
-  config.log_tags = [ :request_id ]
-  config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
-  config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
+	# Logging
+	config.log_tags = [:request_id]
+	config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
+	config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
 
-  # Active Storage
-  config.active_storage.service = :amazon
+	# Active Storage
+	config.active_storage.service = :amazon
 
-  # Active Job
-  config.active_job.queue_adapter = :async
+	# Active Job
+	config.active_job.queue_adapter = :async
 
-  # Mailer
-  config.action_mailer.default_url_options = { host: "yourdomain.com" }
+	# Mailer
+	config.action_mailer.default_url_options = { host: "yourdomain.com" }
 
-  # i18n
-  config.i18n.fallbacks = true
+	# i18n
+	config.i18n.fallbacks = true
 
-  # Schema
-  config.active_record.dump_schema_after_migration = false
+	# Schema
+	config.active_record.dump_schema_after_migration = false
 end
